9bf7172d368b531392fbd8decb3dd35addd42027
syu_asus_Note <syukunt@icloud.com>
2022-11-07 19:49:30 +0900

    【master閉鎖準備】ReactHooksを用いて、Charactorコンポーネント内に、DOM内を端から端へ移動させる関数を設定中。

--- src/Components/Header/Components/Charactor.tsx
+++ src/Components/Header/Components/Charactor.tsx
@@ -1,14 +1,26 @@
-import { type } from "os";
-import React from "react";
+import React, { useState } from "react";
+
 
 type Props = {
     src: string;
     id? : string; 
+    /**初期位置[x, y] */
+    position?: [number, number];
 }
 
 export const Charactor: React.FC<Props> = (props: Props) => {
+
+    //defning the state
+    const position: [number, number] = props.position ? props.position : [0, 0]; //nullish coalescing operator
+    const [charactorPos, setCharactorPos] = useState<[number, number]>(position);
+
+    /**右から左へ移動 */
+    const mvCharacterEdgeFromEdge = (): void => {
+        
+    }
+
     return (
-        <div className="charactor">
+        <div className="charactor" /*onLoad={moveCharactor}*/> //todo: add onload event
             <img src={props.src} alt="charactor" />
         </div>
     );
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { Charactor } from "./Components/Charactor";
+import { moveCharactor } from "./Components/Movement";
 /**Gif import */
 import "../lib/images/buru.gif";
 import "../lib/images/dwarf3.gif";
 
 export const Header:React.FC = () => {
+    const [charactor, setCharactor] = React.useState<string>("Components/lib/images/buru.gif");
     return (
         <div id="header">
             <div className="charactor-container relative bottom-[-35px]">
