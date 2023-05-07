321021852680ea8b3bfd9f08c63f183d118792b7
tsyu12345 <syukunt@icloud.com>
2023-05-07 22:41:09 +0900

    【2023改修】タイトルだけさくっと。Tailwindのかんきょうこうちくいらないのすげぇ。

--- MyHomePage2.0/pages/index.vue
+++ MyHomePage2.0/pages/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <Tutorial/>
+  <div id="container" class="bg-yellow-100 h-screen">
+    <div id="title-area" class="ml-[4%] w-fit pt-12">
+      <h1 class="text-sky-950 font-bold text-4xl">
+        {{ name.first }} <br> {{ name.second }}
+      </h1>
+    </div>
+    <div id="content-area">
+      <div id="twitter-area"></div>
+      <div id="menu-area"></div>
+      <div id="github-grass-area"></div>
+    </div>
+    <div id="photos-area"></div>
+    <div id="footer-area">
+      <div id="link-icons-area"></div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      name: {
+        first: 'SYU',
+        second: 'TAKABAYASHI',
+      }
+    }
+  },
 })
+
 </script>
