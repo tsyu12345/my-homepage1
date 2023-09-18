cc0d652d0d4f0834b867f14e5498b23c8624cfdc
tsyu12345 <syukut@gmail.com>
2023-09-18 20:45:39 +0900

    【2023改修】グリッドレイアウト採用、背景Gifを設定

--- MyHomePage2.0/pages/index.vue
+++ MyHomePage2.0/pages/index.vue
@@ -1,37 +1,37 @@
 <template>
-  <div id="container" class="text-blue-900 bg-yellow-100 w-screen h-screen overflow-hidden">
-    <div id="title-area" class="ml-[4%] w-fit pt-1">
-      <h1 class="text-sky-950 font-bold text-4xl">
-        {{ name.first }} <br> {{ name.second }}
-      </h1>
-    </div>
-    <div id="content-area" class="w-full h-[80%] pl-[60px] mt-5">
-      <div id="TwitteriFrame-area" class="w-[37%] h-fit">
-        <TwitteriFrame />
+  <div class="">
+    <img :src="backgroundGif" class="z-1 blur-md absolute top-0 left-0 w-full h-full object-cover " />
+    <div id="container" class="pl-[4%] z-2 relative text-blue-900 w-screen h-screen overflow-hidden grid-grid-cols-3">
+      <div id="title-area" class="ml-[2%] w-fit pt-1 col-span-3 grid grid-cols-3">
+        <h1 class="text-sky-950 font-bold text-4xl col-span-1">
+          {{ name.first }} <br> {{ name.second }}
+        </h1>
       </div>
-      <div id="menu-area" class="w-fit ml-[44%] relative top-[-440px]">
-        <ul class="h-auto text-cyan-900 text-5xl text-center">
-          <ol v-for="item in menu" :key="item" class="mt-10">
-            <a href="#" class="hover:text-blue-900">{{ item }}</a>
-          </ol>
-        </ul>
+      <div id="content-area" class="w-full h-full col-span-3 grid grid-cols-3">
+        <div id="TwitteriFrame-area" class="h-fit col-span-1 p-10">
+          <TwitteriFrame />
+        </div>
+        <div id="menu-area" class="col-span-1 w-full">
+          <ul class="h-auto text-cyan-900 text-5xl mt-[4.5rem] text-center">
+            <ol v-for="item in menu" :key="item" class="mt-12">
+              <a href="#" class="hover:text-blue-900">{{ item }}</a>
+            </ol>
+          </ul>
+        </div>
+        <div id="footer-area" class="ml-[27%] col-span-2 fixed top-[95%] w-full h-[5%] ">
+          <div id="link-icons-area" class="flex h-fit w-fit m my-auto text-black">
+            <a v-for="link in footLinks" :key="link.name" :href="link.url" class="m-[7%]">
+              <!--<font-awesome-icon :icon="link.icon" />-->
+              <i :class="[link.icon[0], link.icon[1]]" class="fa-2x"></i>
+            </a>
+          </div>
+        </div>
       </div>
-      <div id="github-grass-area" class="w-[60%] h-[30%] border-solid border-2 border-blue-900 relative top-[-40%]">
-        <p class="font-bold text-4xl text-center">GitHub草エリア</p>
+      <div id="photos-area" class="col-span-1 w-1/3 h-full ml-auto absolute top-0 right-0">
+        <ClientOnly>
+          <PhotoViewer />
+        </ClientOnly>
       </div>
-    </div>
-    <div id="footer-area" class="fixed top-[95%] w-full h-[5%] border-b-0 border-r-0 border-l-0 border-solid border-2 border-blue-900">
-      <div id="link-icons-area" class="flex h-fit w-fit mx-auto my-auto text-black">
-        <a v-for="link in footLinks" :key="link.name" :href="link.url" class="m-[7%]">
-          <!--<font-awesome-icon :icon="link.icon" />-->
-          <i :class="[link.icon[0], link.icon[1]]" class="fa-2x"></i>
-        </a>
-      </div>
-    </div>
-    <div id="photos-area" class="w-1/3 h-full ml-auto absolute top-0 right-0">
-      <ClientOnly>
-        <PhotoViewer />
-      </ClientOnly>
     </div>
   </div>
 </template>
@@ -41,28 +41,14 @@ import Vue from 'vue';
 import TwitteriFrame from '~/components/TwitteriFrame.vue';
 import PhotoViewer from '~/components/PhotoViewer.vue';
 import "tw-elements/dist/css/tw-elements.min.css";
-
-/*
-TODO:なぜかfontawesomeが読み込めないので、いずれ直す。現在はCDNで読み込んでいる。
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { library } from '@fortawesome/fontawesome-svg-core';
-//@ts-ignore
-import { faYoutube, faTwitter, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
-
-//@ts-ignore
-library.add(faYoutube, faTwitter, faInstagram, faGithub)
-library.add(faUserSecret as any)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-*/
+//import GitHubGrass from '~/components/GitHubGrass.vue';
 
 export default Vue.extend({
   name: 'IndexPage',
-
   components: {
     TwitteriFrame,
     PhotoViewer,
+    //GitHubGrass,
   },
 
   data() {
@@ -98,13 +84,19 @@ export default Vue.extend({
           icon: ['fa-brands', 'fa-square-github'],
           url: ''
         }
-      ]
+      ],
+      backgroundGif: require('~/static/images/ocean-wave-92-simple-drawing-blue-wave.gif'),
     }
   },
 
   mounted() {
-    //library.add(faUserSecret) //ts-ignore
+    console.log('mounted');
+
   }
 })
-
 </script>
+
+<style type="text/css" scoped="true">
+
+
+</style>
